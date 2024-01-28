e734406466d7d6eef3e483c42555abe6e9f1fb8f
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-28 17:01:25 +0530

    [add] backned url asd remote

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
             '/auth/logout',
             "/search"
         ], {
-            target: 'http://localhost:4500/api/v1',
+            target: 'https://search-track.cyclic.app/api/v1',
             changeOrigin: true,
             onProxyRes: function (proxyRes, req, res) {
                 // Allow requests from any origin (not recommended for production)
