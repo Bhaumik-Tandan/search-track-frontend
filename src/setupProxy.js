8bca547f5254bee7cdaf950f5dfb621b28ec576f
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-29 14:04:52 +0530

    [add] cercel.json

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
             '/auth/logout',
             "/search"
         ], {
-            target: 'http://localhost:4500/api/v1',
+            target: 'https://3b5f-2405-201-13-305f-fd4e-21a6-6c6a-e6b.ngrok-free.app/api/v1',
             changeOrigin: true,
             onProxyRes: function (proxyRes, req, res) {
                 // Allow requests from any origin (not recommended for production)
